6f335d9f1e5a208f2ba8cdb461ef1a06bf4b2c3c
Dans182 <dgaiteiro@gmail.com>
2022-04-02 21:39:21 +0000

    bootstrap listo

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -3,16 +3,20 @@ import propTypes from "prop-types";
 
 const TrafficLight = (props) => {
 	return (
-		<div className="card text-white bg-dark m-1" style={{ Width: "10rem" }}>
+		<div className="card bg-dark" style={{ width: "5rem" }}>
 			<div className="card-body d-flex justify-content-center text-center">
-				<h1 className="card-title">{props.icono}</h1>
+				<h1 className="card-title m-0">
+					{props.icono}
+					{props.icono2}
+					{props.icono3}
+				</h1>
 			</div>
 		</div>
 	);
 };
 
 TrafficLight.propTypes = {
-	icono: propTypes.string,
+	icono: propTypes.object,
 };
 
 export default TrafficLight;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,15 +2,19 @@ import React, { useState, useEffect } from "react";
 import TrafficLight from "../component/TrafficLight.jsx";
 
 //create your first component
-const Home = () => {
-	let luzRoja = <i className="fas fa-circle"></i>;
-	let luzAmarilla = <i className="fas fa-circle"></i>;
-	let luzVerde = <i className="fas fa-circle"></i>;
+let luzRoja = <i className="fas fa-circle text-danger"></i>;
+let luzAmarilla = <i className="fas fa-circle text-warning"></i>;
+let luzVerde = <i className="fas fa-circle text-success"></i>;
 
+const Home = () => {
 	return (
-		<div className="container mt-2">
+		<div className="container mt-5">
 			<div className="row justify-content-center">
-				<TrafficLight icono={clock} />
+				<TrafficLight
+					icono={luzRoja}
+					icono2={luzAmarilla}
+					icono3={luzVerde}
+				/>
 			</div>
 		</div>
 	);
