5dcd99aea407a05feb114d27bbfad2029daf6e30
Dans182 <dgaiteiro@gmail.com>
2022-04-03 13:39:13 +0000

    ejercicio resuelto

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -3,14 +3,15 @@ import propTypes from "prop-types";
 
 const TrafficLight = (props) => {
 	return (
-		<div className="row justify-content-center">
-			<div className="card bg-dark" style={{ width: "5rem" }}>
-				<div className="card-body d-flex justify-content-center text-center">
-					<h1 className="card-title m-0">
-						{props.icono}
-						{props.icono2}
-						{props.icono3}
-					</h1>
+		<div className="container mt-5">
+			<div className="row justify-content-center">
+				<div
+					className="card bg-dark"
+					style={{ width: "5rem", height: "15rem" }}>
+					<div className="card-body d-flex justify-content-center text-center">
+						<h1 className="card-title m-0"></h1>
+						<div>{props.icono}</div>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,19 +1,58 @@
 import React, { useState } from "react";
-import TrafficLight from "../component/TrafficLight.jsx";
 
 //create your first component
 let luzRoja = <i className="fas fa-circle text-danger"></i>;
 let luzAmarilla = <i className="fas fa-circle text-warning"></i>;
 let luzVerde = <i className="fas fa-circle text-success"></i>;
+let luzPruebaRojo = <i className="prueba1 fas fa-circle text-danger"></i>;
+let luzPruebaAmarillo = <i className="prueba2 fas fa-circle text-warning"></i>;
+let luzPruebaVerde = <i className="prueba3 fas fa-circle text-success"></i>;
 
 const Home = () => {
+	const [seleccion, setSeleccion] = useState(luzRoja);
+	const [seleccion2, setSeleccion2] = useState(luzAmarilla);
+	const [seleccion3, setSeleccion3] = useState(luzVerde);
+
 	return (
 		<div className="container mt-5">
-			<TrafficLight
-				icono={luzRoja}
-				icono2={luzAmarilla}
-				icono3={luzVerde}
-			/>
+			<div className="row justify-content-center">
+				<div
+					className="card bg-dark justify-content-center "
+					style={{ width: "5rem", height: "15rem" }}>
+					<div
+						className="justify-content-center d-flex"
+						onClick={() =>
+							setSeleccion(
+								seleccion == luzRoja ? luzPruebaRojo : luzRoja
+							)
+						}>
+						{seleccion}
+					</div>
+
+					<div
+						className="justify-content-center d-flex"
+						onClick={() =>
+							setSeleccion2(
+								seleccion2 == luzAmarilla
+									? luzPruebaAmarillo
+									: luzAmarilla
+							)
+						}>
+						{seleccion2}
+					</div>
+					<div
+						className="justify-content-center d-flex"
+						onClick={() =>
+							setSeleccion3(
+								seleccion3 == luzVerde
+									? luzPruebaVerde
+									: luzVerde
+							)
+						}>
+						{seleccion3}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
